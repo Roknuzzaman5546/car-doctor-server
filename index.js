c14fa6d9a5617a596cdbe7647faba321b55bd57f
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2023-10-28 01:58:15 +0600

    Orders data added

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function run() {
         await client.connect();
 
         const servicescollection = client.db('doctordb').collection('services')
-        const chekoutcollection = client.db('doctordb').collection('order')
+        const orderscollection = client.db('doctordb').collection('order')
 
         app.get('/services', async (req, res) => {
             const cursor = servicescollection.find()
@@ -40,14 +40,26 @@ async function run() {
             const quary = { _id: new ObjectId(id) }
             const options = {
                  // Include only the `title` and `imdb` fields in each returned document
-                projection: { title: 1, price: 1, service_id: 1 },
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
             const result = await servicescollection.findOne(quary, options)
             res.send(result)
         })
 
-        app.post('/orders', (req, res) =>{
-            const orders = req.params.body
+        app.get('/orders', async (req, res) =>{
+            let query = {};
+            if (req.query?.email) {
+                query = {email : req.query.email}
+            }
+            const result = await orderscollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post('/orders', async (req, res) =>{
+            const orders = req.body
+            console.log(orders)
+            const result = await orderscollection.insertOne(orders)
+            res.send(result)
         })
 
 
