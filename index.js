ee0c2afda4ce5378a51cc652256ccc838301f734
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2023-10-29 11:10:54 +0600

    new axios code added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const cors = require('cors');
 const app = express()
@@ -29,6 +30,15 @@ async function run() {
         const servicescollection = client.db('doctordb').collection('services')
         const orderscollection = client.db('doctordb').collection('order')
 
+        // token related
+
+        app.post('/jwt', (req, res) =>{
+            const user = req.body;
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn : '1h'})
+            res.send(token)
+        })
+
         app.get('/services', async (req, res) => {
             const cursor = servicescollection.find()
             const result = await cursor.toArray()
@@ -39,26 +49,48 @@ async function run() {
             const id = req.params.id;
             const quary = { _id: new ObjectId(id) }
             const options = {
-                 // Include only the `title` and `imdb` fields in each returned document
+                // Include only the `title` and `imdb` fields in each returned document
                 projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
             const result = await servicescollection.findOne(quary, options)
             res.send(result)
         })
 
-        app.get('/orders', async (req, res) =>{
+        app.get('/orders', async (req, res) => {
             let query = {};
             if (req.query?.email) {
-                query = {email : req.query.email}
+                query = { email: req.query.email }
             }
             const result = await orderscollection.find(query).toArray()
             res.send(result)
         })
 
-        app.post('/orders', async (req, res) =>{
+        app.post('/orders', async (req, res) => {
             const orders = req.body
             console.log(orders)
             const result = await orderscollection.insertOne(orders)
+            res.send(result)
+        })
+
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const ordersupdate = req.body;
+            console.log(ordersupdate)
+            const updateDoc = {
+                $set: {
+                    status: ordersupdate.status 
+                },
+              };
+            const result = await orderscollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await orderscollection.deleteOne(query)
             res.send(result)
         })
 
